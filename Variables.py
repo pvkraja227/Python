9058db760f759ceeae6c715ec8e6d559c0b23d95
pvkraja227 <Venkat.klce227@gmail.com>
2025-03-14 02:16:01 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,28 @@
+# a=len("hello, world!")
+# print(a)
+
+# print(len("raja"))
+
+# print(len(input()))
+# print(len(input("name?")))
+
+# a=input("a:")
+# b=input("b:")
+# c=a
+# a=b
+# b=c
+# print("a:",a)
+# print("b:",b)
+# print("a:"+a +" " "b:"+b)
+
+# print("Welcome to the band.")
+# street=input("What is your street?")
+# pet=input("What is your pet?")
+# print("Your band name could be "+street+" "+pet)
+
+# print(len([1,2,3])) o/p: 3
+# print(len(123)) error
+# print(len("123")) o/p: 3
+
+
+
