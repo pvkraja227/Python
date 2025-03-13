73144898ece1f0212cc2aba86e76d2c862575a04
pvkraja227 <Venkat.klce227@gmail.com>
2025-03-14 02:23:14 +0530

    Create Random_Variables.py

---
+++ Random_Variables.py
@@ -0,0 +1,29 @@
+# import random
+# y=random.randint(0,100) # prints any number b/w 1 to 100 both inclusive
+# print(y)
+
+# import random
+# import 1_if_else # importing a value from a different .py
+# y=random.randint(0,100)
+# print(y)
+# print(1_if_else.my_favourite_num)
+
+# import random
+# y=random.random() * 10 # prints 0 <= y < 1
+# print(y)
+
+# import random
+# y=random.uniform(1, 10) # prints 0 <= y <= 1
+# print(y)
+
+# import random
+# y=random.randint(0,1)
+# if y == 0:
+#     print("TAILS")
+# else:
+#     print("HEADS")
+
+# float b/w 0 to 5
+# import random
+# y=random.random() * 5 # prints 0 <= y < 1 * 5
+# print(y)
