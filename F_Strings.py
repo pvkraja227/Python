25aed6f8568a75c632e4047995e04803d0ba632e
pvkraja227 <Venkat.klce227@gmail.com>
2025-03-14 02:18:09 +0530

    Create F_Strings.py

---
+++ F_Strings.py
@@ -0,0 +1,21 @@
+# combining different types of data strings
+
+# score=0
+# height=1.8
+# is_winning=True
+# print(f"Your score is {score}, your height is {height}, your chances of winning is {is_winning}")
+# print("Your score is", str(score), "your height is", str(height), "your chances of winning is", str(is_winning))
+# print("Your score is "+ str(score)+ "your height is "+ str(height)+ "your chances of winning is "+ str(is_winning))
+
+# how many weeks left until you are 90 years old
+# age=input("enter your age in years: ")
+# x=90*52 - int(age)*52
+# print(f"weeks left until 90 years:", {x})
+
+# tip calculator
+# print("Welcome to tip calculator")
+# x=float(input("total bill: Rs "))
+# y=int(input("tip amount: 10, 12 or 15: "))
+# p=int(input("how many persons: "))
+# total=round((x+y)/p,2)
+# print(f"each person's amount is {total}")
