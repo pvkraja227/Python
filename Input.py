7fe65dad44cd803ee4e421c57e5f9b5f02c4a05f
pvkraja227 <Venkat.klce227@gmail.com>
2025-03-14 02:17:11 +0530

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,20 @@
+# a=input()
+# b=input()
+# print(a+b)
+
+# num1=float(input())
+# num2=int(input())
+# add=num1 * num2
+# print(add)
+
+# a=3.4
+# b=3.987
+# add=a+b
+# print(add)
+
+# a=int(input())
+# b=int(input())
+# print(a+b)
+# print(b+a)
+
+
